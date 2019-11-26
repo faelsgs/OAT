5c873759bf558bbeacffa16acc5bc6846a732801
faelsgs <50033756+faelsgs@users.noreply.github.com>
2019-11-25 21:54:13 -0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  { 
+    path: 'about', 
+    loadChildren: './about/about.module#AboutPageModule' 
+  },
+  { 
+    path: 'listagem', 
+    loadChildren: './listagem/listagem.module#ListagemPageModule' 
+  },
+  { 
+    path: 'formulario', 
+    loadChildren: './formulario/formulario.module#FormularioPageModule' 
+  },
+  { 
+    path: 'modal', 
+    loadChildren: './modal/modal.module#ModalPageModule' 
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
